c828568dcdaf0eb76f280027a619d4bfe35adaff
Thaís Campos Martins <51833404+Thcamposmartins@users.noreply.github.com>
2021-01-21 17:43:54 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://thcampos:thcampos_@nodeform.aaxwt.mongodb.net/nodeteste?retryWrites=true&w=majority", {useMongoClient:true, useNewUrlParser: true,  useUnifiedTopology: true});
+mongoose.connect("mongodb+srv", {useMongoClient:true, useNewUrlParser: true,  useUnifiedTopology: true});
 mongoose.Promise = global.Promise;
 
 
